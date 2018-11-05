560984d34757a74ef35a0d71e5f96af609004475
Little-Captain <coder.little-captain@gmail.com>
2018-11-05 23:21:00 +0800

    添加倒计时label

--- Math Quiz/Form1.Designer.cs
+++ Math Quiz/Form1.Designer.cs
@@ -28,22 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.timeLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(0, 0);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(41, 12);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 362);
+            this.Controls.Add(this.timeLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Math Quiz";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timeLabel;
     }
 }
 
