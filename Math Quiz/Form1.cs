77f62bbf1652635d168074eb9163f65397032311
Little-Captain <coder.little-captain@gmail.com>
2018-11-06 12:43:35 +0800

    加法计算

--- Math Quiz/Form1.cs
+++ Math Quiz/Form1.cs
@@ -11,9 +11,69 @@ namespace Math_Quiz
 {
     public partial class Form1 : Form
     {
+        Random randomizer = new Random();
+
+        int addend1;
+        int addend2;
+
+        int timeLeft;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void StartTheQuiz()
+        {
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            sum.Value = 0;
+
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!", "Congratulations");
+                startButton.Enabled = true;
+            } else if (timeLeft > 0)
+            {
+                timeLeft -= 1;
+                timeLabel.Text = timeLeft + "seconds";
+            } else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                startButton.Enabled = true;
+            }
+        }
+
+        private bool CheckTheAnswer()
+        {
+            if (addend1 + addend2 == sum.Value)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
         }
     }
 }
